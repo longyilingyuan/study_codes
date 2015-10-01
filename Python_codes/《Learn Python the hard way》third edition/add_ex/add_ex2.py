9d84e7296aeed6c35f091ffc8ec80bb14960ac19
longyilingyuan <longyilingyuan@163.com>
2015-10-01 16:50:29 +0800

    Signed-off-by: longyilingyuan <longyilingyuan@163.com>

---
+++ Python_codes/《Learn Python the hard way》third edition/add_ex/add_ex2.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# 在此例中，在""中使用""，出现语法错误，使用转义字符\"（即在"加上\符号）可解决。 
+print "Hello World! Hello Again I like typing this.This is fun.Yay! Printing.I'd much rather you 'not'.I \"said\" do not touch this.2015_9_23"
+ 
+ 
+ 
+ 
+ 
+ 
